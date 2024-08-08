876b234a4703e8b9ef84203091cca8eed88e94a8
gonzivaser <gonzalovaserman@gmail.com>
2024-08-08 09:40:42 -0300

    Pense que terminado PulpFiction, me falta el punto 7 que es imposible

--- Logico/Pulp Fiction/pulpFiction.pl
+++ Logico/Pulp Fiction/pulpFiction.pl
@@ -157,10 +157,17 @@ esRespetable(Personaje) :-
 
 /*-------------------------------------------------- PUNTO 7 ---------------------------------------------------------*/
 
+/*7. hartoDe/2: un personaje está harto de otro, cuando todas las tareas asignadas al primero 
+requieren interactuar con el segundo (cuidar, buscar o ayudar) o un amigo del segundo. Ejemplo:
+
+? hartoDe(winston, vincent).
+true % winston tiene que ayudar a vincent, y a jules, que es amigo de vincent. 
+*/
+
 hartoDe(Personaje, OtroPersonaje) :-
     sonPersonajes(Personaje, OtroPersonaje),
-    Personaje \= OtroPersonaje,
-    forall(encargo(Personaje, _, Tarea), requiereInteractuarConOtroPersonaje(Tarea, OtroPersonaje)).
+    forall(encargo(Personaje, _, Tarea), requiereInteractuarConOtroPersonaje(Tarea, OtroPersonaje)), 
+    Personaje \= OtroPersonaje.
 
 requiereInteractuarConOtroPersonaje(cuidar(OtroPersonaje), OtroPersonaje).
 
@@ -168,8 +175,10 @@ requiereInteractuarConOtroPersonaje(ayudar(OtroPersonaje), OtroPersonaje).
 
 requiereInteractuarConOtroPersonaje(buscar(OtroPersonaje, _), OtroPersonaje).
 
-requiereInteractuarConOtroPersonaje(_, OtroPersonaje) :-
-    amigo(OtroPersonaje, _).
+requiereInteractuarConOtroPersonaje(Tarea, OtroPersonaje) :-
+    amigo(OtroPersonaje, Amigo),
+    requiereInteractuarConOtroPersonaje(Tarea, Amigo).
+
 
 /*-------------------------------------------------- PUNTO 8 ---------------------------------------------------------*/
 
